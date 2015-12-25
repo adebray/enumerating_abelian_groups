6617b3934c99526022b117230f19bf6bb2fe0455
Arun Debray <a.debray@gmail.com>
2015-12-24 17:53:27 -0800

    First commit: have primary components for Python

---
+++ README.md
@@ -0,0 +1,12 @@
+As a consequence of the [fundamental theorem of finitely generated abelian groups](https://en.wikipedia.org/wiki/Finitely_generated_abelian_group), if *G* is a finite abelian group, it can be written as a direct product (or, equivalently, a direct
+sum) of finite cyclic groups, which are all isomorphic to the integers mod *n* for some *n*. Moreover, one can choose
+the *n* appearing in this decomposition to be particularly nice: either a list of powers of primes or a list of *invariant
+factors*, where the first one divides the second one, which divides the third one, and so on. The two are related by way
+of the [Chinese remainder theorem](https://en.wikipedia.org/wiki/Chinese_remainder_theorem).
+
+This is simple enough that one can turn the problem around: what is the list of all finite abelian groups of a given order?
+The answer is given by listing the prime factors of the order and putting them together either using prime powers or
+invariant factors. This is relatively algorithmic, so I decided to automate it, partly to get more experience with these
+algorithms and partly to play with some interesting programming problems.
+
+In addition to the sources for these programs, I will also provide some sample outputs.
---
+++ finite_abelian_groups.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3.5
+
+# Arun Debray, 24 Dec. 2015
+# Given a group order, classifies finite abelian groups of that order.
+
+# ./finite_abelian_groups.py [-tpi] number
+#	-t formats the output in TeX (as opposed to in the terminal)
+#	-p chooses the primary components decomposition (default)
+#	-i chooses the invariant factors decomposition
+
+import argparse
+import collections
+import functools
+import itertools
+import math
+
+# Handles command-line arguments. See usage, above.
+def parse_args() -> argparse.Namespace:
+	parser = argparse.ArgumentParser(description='Classifies finite abelian groups of a given order.')
+	parser.add_argument('-t', action='store_true', help='formats output in LaTeX')
+	parser.add_argument('-p', action='store_true', help='chooses primary components decomposition (default)')
+	parser.add_argument('-i', action='store_true', help='chooses invariant factors decomposition')
+	parser.add_argument('order', metavar='n', type=int, help='group order')
+	return parser.parse_args()
+
+# Determines the prime factors of a number. No, this isn't the best algorithm, but
+# it's good enough. Returns them as a Counter object (a dict of prime -> power), in which
+# all values will be strictly positive.
+def prime_factors(n: int) -> collections.Counter:
+	for i in range(2, 1 + math.ceil(math.sqrt(n))):
+		# By doing this in order, we guarantee that this only happens when i is prime (2 comes before
+		# any other even number, etc.)
+		if n % i == 0:
+			if n == i:
+				return collections.Counter({n: 1})
+			else:
+				to_return = prime_factors(n // i)
+				to_return[i] += 1
+				return to_return
+	# if n is prime
+	return collections.Counter({n: 1})
+
+# A helper function for the partitions function, below. Returns the partitions of n using integers
+# less than or equal to m. However, within this program it makes more sense to do this multiplicatively:
+# n is represented as p^n, and we are partitioning as p^n = p^{n_1} x p^{n_2} x ... x p^{n_k}.
+@functools.lru_cache(maxsize=None) # Wraps the function in a cache, making memoization simpler
+def _part_lt(p: int, n: int, m: int) -> list:
+	if n == 0:
+		return [[]]
+	if n == 1:
+		return [[p]]
+	if m > n:
+		return _part_lt(p, n, n)
+	# Now, we recurse: the first entry in the partition can be any j in {1, ..., m}, and the rest is a
+	# partition in _part_lt(n-j, j).
+	to_return = []
+	for j in range(1, m+1):
+		to_return += [part + [p**j] for part in _part_lt(p, n-j, j)]
+	return to_return
+
+# Returns the partitions of n as pth powers, i.e. the ways of writing p^n = p^{a_1} x ... x p^{a_m}
+# such that  each a_i is a positive integer and a_i >= a_{i+1} for each i. This is the algorithmic meat
+# of each decomposition, though some thought must go into piecing the partitions for different primes
+# together. Of course, this function calls the helper function, above.
+def partitions(p: int, n: int) -> list:
+	return _part_lt(p, n, n)
+
+# Flattens one level of a list, turning [[1, 2, 3], [4, 5]] into [1, 2, 3, 4, 5].
+def flatten(a: list) -> list:
+	return [item for sublist in a for item in sublist]
+
+# Given the prime factors, returns a list of all abelian groups of the given order in primary-
+# factors format.
+def primary_factor_decomp(factors: collections.Counter) -> list:
+	decomps_at_primes = [partitions(p, factors[p]) for p in factors]
+	return [flatten(choice) for choice in itertools.product(*decomps_at_primes)]
+
+# note that this also uses partitions of pth powers, but puts them together in a different way.
+def invariant_factor_decomp(factors: collections.Counter) -> list:
+	pass # TODO
+
+# Formats and prints the output.
+# TODO: factor out common code, "is"/"are"
+def output(groups: list, order: int, as_TeX: bool):
+	if as_TeX:
+		print('\\documentclass{amsart}')
+		print('\\newcommand{\\Z}{\\mathbb Z}')
+		print('\\title{Groups of Order %d}' % order)
+		print('\\begin{document}')
+		print('\\maketitle')
+		print('There are %d abelian groups of order %d.' % (len(groups), order))
+		print('\\begin{gather*}')
+		print('\\\\\n'.join(['\\oplus'.join(['\\Z/%d' % component for component in group]) for group in groups]))
+		print('\\end{gather*}')
+		print('\\end{document}')
+	else:
+		print('There are %d abelian groups of order %d.' % (len(groups), order))
+		for group in groups:
+			print('⊕ '.join('\033[1mZ\033[0m/%d' % component for component in group))
+
+def main():
+	arginfo = parse_args()
+	groups = None
+	factors = prime_factors(arginfo.order)
+	if arginfo.i:
+		groups = invariant_factor_decomp(factors)
+	else:
+		groups = primary_factor_decomp(factors)
+	output(groups, arginfo.order, arginfo.t)
+
+if __name__ == '__main__':
+	main()
